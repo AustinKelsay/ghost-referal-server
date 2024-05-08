6d019d94fa6cb734a9d1963d8987f9429f1f89ff
austinkelsay <austinkelsay@yahoo.com>
2024-05-08 15:49:05 -0500

    using global instance of prisma

--- db/methods/referralMethods.js
+++ db/methods/referralMethods.js
@@ -1,6 +1,4 @@
-const { PrismaClient } = require('@prisma/client');
-
-const prisma = new PrismaClient();
+const prisma = require('../prisma.js');
 
 const createReferral = async (referrerName, referrerEmail, refereeEmail) => {
     try {
