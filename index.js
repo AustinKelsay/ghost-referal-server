a98dad11c96247bd410e05e36e893d2e751a67e9
austinkelsay <austinkelsay@yahoo.com>
2024-05-09 14:52:57 -0500

    Remove auth middleware

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const referralRouter = require('./routers/referralRouter');
 const cronRouter = require('./routers/cronRouter');
 const { errorMiddleware } = require('./middleware/errorMiddleware');
-const { authMiddleware } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -12,7 +11,7 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.use('/referral', authMiddleware, referralRouter);
+app.use('/referral', referralRouter);
 app.use('/cron', cronRouter);
 
 // Error handling middleware
