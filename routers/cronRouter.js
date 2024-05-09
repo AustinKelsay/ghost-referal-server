92d448de388097624097ed28e95cd2f0df1216ac
austinkelsay <austinkelsay@yahoo.com>
2024-05-09 14:55:08 -0500

    referees might not exist

--- routers/cronRouter.js
+++ routers/cronRouter.js
@@ -10,10 +10,10 @@ router.get('/', async (req, res, next) => {
     const token = await createGhostJWT();
     const referees = await getAllReferees();
 
-    if (referees.length === 0) {
+    if (referees?.length === 0) {
       return res.status(404).json({ message: 'No referees found' });
     }
-    
+
     console.log('Referees:', referees);
 
     const response = await axios.get(`${GHOST_API}/members/`, {
