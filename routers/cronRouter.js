c9643f133032b56f81bce40b32cb7edcf71abb38
austinkelsay <austinkelsay@yahoo.com>
2024-05-10 15:16:20 -0500

    Add token endpoint rq

--- routers/cronRouter.js
+++ routers/cronRouter.js
@@ -75,21 +75,23 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-// router.post('/', async (req, res, next) => {
-//     try {
-//       const { referrerName, referrerEmail, refereeEmail } = req.body;
-//       const emailResponse = await sendRewardEmail(refereeEmail, referrerEmail);
-//       console.log('Email response on endpoint:', emailResponse);
-//       if (emailResponse.error) {
-//         const error = new Error('Error sending email');
-//         error.statusCode = 500;
-//         throw error;
-//       } else {
-//         return res.status(200).send(emailResponse);
-//       }
-//     } catch (error) {
-//       return next(error);
-//     }
-//   });
+router.post('/', async (req, res, next) => {
+    const token = await createGhostJWT();
+    res.status(200).send({ message: 'Endpoint hit', token });
+    // try {
+    //   const { referrerName, referrerEmail, refereeEmail } = req.body;
+    //   const emailResponse = await sendRewardEmail(refereeEmail, referrerEmail);
+    //   console.log('Email response on endpoint:', emailResponse);
+    //   if (emailResponse.error) {
+    //     const error = new Error('Error sending email');
+    //     error.statusCode = 500;
+    //     throw error;
+    //   } else {
+    //     return res.status(200).send(emailResponse);
+    //   }
+    // } catch (error) {
+    //   return next(error);
+    // }
+  });
 
 module.exports = router;
