fce1533341e1178c5f2cad2bebbe1138f50b7db5
austinkelsay <austinkelsay@yahoo.com>
2024-05-11 16:33:19 -0500

    proxy:true

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 500, // Limit each IP to 500 requests per windowMs
   message: 'Too many requests from this IP, please try again later',
-  trustProxy: true
+  trustProxy: true,
+  proxy: true
 });
 
 app.use(limiter);
