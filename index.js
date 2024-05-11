3c45a679dc94202c2c1be920caa707832b003008
austinkelsay <austinkelsay@yahoo.com>
2024-05-11 16:29:21 -0500

    trustProxy in prod since vercel adds x-forwarded-for header

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 500, // Limit each IP to 500 requests per windowMs
   message: 'Too many requests from this IP, please try again later',
-  enableXForwardedFor: true,
+  trustProxy: true
 });
 
 app.use(limiter);
