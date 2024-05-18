34e8e4ea9ea8ff380e60be49e33bf2206c8e91bd
austinkelsay <austinkelsay@yahoo.com>
2024-05-18 15:31:11 -0500

    Revert post endpoint

--- routers/cronRouter.js
+++ routers/cronRouter.js
@@ -76,20 +76,20 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res, next) => {
-    try {
-      const { referrerName, referrerEmail, refereeEmail } = req.body;
-      const emailResponse = await sendReferredEmail(refereeEmail, referrerName);
-      if (emailResponse) {
-        return res.status(200).send(emailResponse);
-      }
-    } catch (error) {
-      console.error('Error sending referred email:', error.message, error.stack, {
-        requestData: req.body,
-        responseData: error.response?.data,
-      });
-      return next(error);
-    }
-  });
+// router.post('/', async (req, res, next) => {
+//     try {
+//       const { referrerName, referrerEmail, refereeEmail } = req.body;
+//       const emailResponse = await sendReferredEmail(refereeEmail, referrerName);
+//       if (emailResponse) {
+//         return res.status(200).send(emailResponse);
+//       }
+//     } catch (error) {
+//       console.error('Error sending referred email:', error.message, error.stack, {
+//         requestData: req.body,
+//         responseData: error.response?.data,
+//       });
+//       return next(error);
+//     }
+//   });
 
 module.exports = router;
