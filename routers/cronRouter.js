9eb2f2f96a0112ad619e8cc6c7dfa5a137ffdf85
austinkelsay <austinkelsay@yahoo.com>
2024-05-23 10:19:37 -0500

    Add special log

--- routers/cronRouter.js
+++ routers/cronRouter.js
@@ -38,6 +38,10 @@ router.get('/', async (req, res, next) => {
             
             if (response.data?.members && response.data?.members.length > 0) {
                 const member = response.data.members[0];
+
+                if (member.email === "parkeralewis@gmail.com") {
+                    console.log('parker:', member);
+                }
                 
                 if (member.email_opened_count >= 4) {
                     eligibleReferees.push(referee);
