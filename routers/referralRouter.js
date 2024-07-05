0a3b6ff9fb714b964679a3304786d97f6790b1d6
austinkelsay <austinkelsay@yahoo.com>
2024-07-05 16:46:50 -0500

    Add server side recaptcha integration for posting referals

--- routers/referralRouter.js
+++ routers/referralRouter.js
@@ -6,10 +6,20 @@ const { createReferral } = require('../db/methods/referralMethods');
 const { fetchMemberByEmail } = require('../scripts/ghost/fetchMemberByEmail');
 const {sendReferredEmail} = require('../scripts/ghost/sendReferredEmail');
 const GHOST_API = process.env.GHOST_API;
+const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY;
 
 router.post('/', async (req, res, next) => {
   try {
-    const { referrerName, referrerEmail, refereeEmail } = req.body;
+    const { referrerName, referrerEmail, refereeEmail, recaptchaToken } = req.body;
+
+    // Verify reCAPTCHA token
+    const recaptchaResponse = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${RECAPTCHA_SECRET_KEY}&response=${recaptchaToken}`);
+    if (!recaptchaResponse.data.success) {
+      const error = new Error('reCAPTCHA validation failed.');
+      error.statusCode = 400;
+      throw error;
+    }
+
     const token = await createGhostJWT();
 
     const refereeStatus = await fetchMemberByEmail(refereeEmail, token);
